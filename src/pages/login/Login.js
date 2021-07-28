098d670dd45ac09a2db3abb25fc89eec6c9c49b7
Kacper <hernackikacper@gmail.com>
2021-07-28 21:10:49 +0200

    login with mail and google

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,66 +1,96 @@
-import React, { useState } from 'react'
-import {NavLink, useHistory} from 'react-router-dom'
-import Footer from '../../components/Footer/Footer'
-import './Login.css'
+import React, { useState } from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
+import Footer from '../../components/Footer/Footer';
+import './Login.css';
+import { db, auth, provider } from '../../firebase';
 
 function Login() {
-    const [Name, setName] = useState("")
-    const [Password, setPassword] = useState("")
-    const history = useHistory()
+  const [mail, setMail] = useState('');
+  const [password, setPassword] = useState('');
+  const history = useHistory();
 
-    const handleClick = (e) => {
-        e.preventDefault()
+  const logInGoogle = () => {
+    //gmail popup
+    auth
+      .signInWithPopup(provider)
+      .then((cred) => {
+        //adding user to the database
+        return db.collection('users').doc(cred.user.uid).set({
+          name: cred.user.displayName,
+          id: cred.user.uid,
+        });
+      })
+      .catch((error) => alert(error.message));
 
-        // post request 
+    history.push('/');
+  };
 
-        // if response get then redirect
-        history.push('./')
-    }
-    return (
-        <>
-        <div className="login">
-           <div className="login_text">
-                <h1>Welcome back!</h1>
-                <p>By logging into profese, you agree to our <a href="#">Terms of Use</a> and <a href="#">Privacy Policy.</a></p>
-           </div>
+  const logInFacebook = () => {};
 
-           <div className="login_input">
-                <div className="social_login">
-                   <button>Continue with Google</button>
-                   <button>Continue with Facebook</button>
-                </div>
+  const logIn = (e) => {
+    e.preventDefault();
 
-                <div className="manual_login">
-                    <label htmlFor="username">Email or Username</label>
-                    <input 
-                       type="text"
-                       name="username"
-                       id="username"
-                       value={Name}
-                       onChange={(e) => {setName(e.target.value)}}
-                        />
+    //request with email and password
+    auth
+      .signInWithEmailAndPassword(mail, password)
+      .catch((error) => alert(error.message));
 
-                    
-                    <label htmlFor="password">Password</label>
-                    <input 
-                       type="password"
-                       name="password"
-                       id="password"
-                       value={Password}
-                       onChange={(e) => {setPassword(e.target.value)}}
-                        />
-
-                    <label>Forgot Password?</label>
-                    <button onClick={handleClick} className="login_btn">Log in</button>
-
-                    <NavLink to="/signup">Create an account</NavLink>
-                </div>
-           </div>
+    // if response get then redirect
+    history.push('./');
+  };
+  return (
+    <>
+      <div className="login">
+        <div className="login_text">
+          <h1>Welcome back!</h1>
+          <p>
+            By logging into profese, you agree to our{' '}
+            <a href="#">Terms of Use</a> and <a href="#">Privacy Policy.</a>
+          </p>
         </div>
 
-        <Footer />
+        <div className="login_input">
+          <div className="social_login">
+            <button onClick={logInGoogle}>Continue with Google</button>
+            <button onClick={logInFacebook}>Continue with Facebook</button>
+          </div>
+
+          <div className="manual_login">
+            <label htmlFor="username">Email</label>
+            <input
+              type="text"
+              name="username"
+              id="username"
+              value={mail}
+              onChange={(e) => {
+                setMail(e.target.value);
+              }}
+            />
+
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              name="password"
+              id="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+
+            <label>Forgot Password?</label>
+            <button onClick={logIn} className="login_btn">
+              Log in
+            </button>
+
+            <NavLink to="/signup">Create an account</NavLink>
+          </div>
+        </div>
+      </div>
+
+      <Footer />
     </>
-    )
+  );
 }
 
-export default Login
+export default Login;
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,120 +1,154 @@
-import React, { useState } from 'react'
-import Footer from '../../components/Footer/Footer'
-import './signup.css'
-import {NavLink, useHistory} from 'react-router-dom'
+import React, { useState } from 'react';
+import Footer from '../../components/Footer/Footer';
+import './signup.css';
+import { NavLink, useHistory } from 'react-router-dom';
+import { db, auth, provider } from '../../firebase';
 
 export default function Signup() {
-    const [email, setemail] = useState("")
-    const [Name, setName] = useState("")
-    const [Password, setPassword] = useState("")
-    const [birth_date, setbirth_date] = useState("")
-    const [Profile, setProfile] = useState("")
-    const history = useHistory()
-    
+  const [email, setemail] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const [birth_date, setbirth_date] = useState('');
+  const [profile, setProfile] = useState('');
+  const history = useHistory();
 
-    const validate = () => {
-       const mail_reg = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-       
-       if( birth_date === "" || email === "" || Password === "" || Profile === "" || Name === ""){
-            alert("Empty fields not allowed....!!!")
-            return false
-        }
-       else if(!mail_reg.test(email)){
-            alert("Enter valid email...!!!")
-            return false
-       }
-       else if(Password.length < 8){
-            alert("Password length must be greater than 8...!!!")
-            return false
-       }
-       else return true
-    }
+  const validate = () => {
+    const mail_reg =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-    const register = (e) => {
-        e.preventDefault()
-        validate() // for input validation 
+    if (
+      birth_date === '' ||
+      email === '' ||
+      password === '' ||
+      profile === '' ||
+      name === ''
+    ) {
+      alert('Empty fields not allowed....!!!');
+      return false;
+    } else if (!mail_reg.test(email)) {
+      alert('Enter valid email...!!!');
+      return false;
+    } else if (password.length < 8) {
+      alert('Password length must be greater than 8...!!!');
+      return false;
+    } else return true;
+  };
 
+  const register = (e) => {
+    e.preventDefault();
+    validate(); // for input validation
 
-        // post request if validate is true
+    // post request if validate is true
+    auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+      return db.collection('users').doc(cred.user.uid).set({
+        email: email,
+        id: cred.user.uid,
+        birth_date: birth_date,
+        profile: profile,
+        name: name,
+      });
+    });
+    // redirect to home page
+    history.push('./');
+  };
+  return (
+    <>
+      <div className="signup">
+        <div className="signup_sec1">
+          <h1>ENHANCE YOUR LEARNING AND EXPERIENCE, ANYTIME, ANYWHERE</h1>
 
-        // redirect to home page
-        history.push('./')
-    }
-    return (
-        <>
-        <div className="signup">
-            <div className="signup_sec1">
-                <h1>ENHANCE YOUR LEARNING AND EXPERIENCE, ANYTIME, ANYWHERE</h1>
-
-                <p>Join profese to get 1-on-1 online help for courses and assignments.</p>
-                <p>By logging into profese, you agree to our <a href="#">Terms of Use</a> and <a href="#">Privacy Policy.</a></p>
-            </div>
-       
-            <div className="signup_sec2">
-              <form onSubmit={register}>
-                <div className="signup_profile_choice">
-                    <label htmlFor="profile">Join profese as a</label>
-                    <div className="choices">
-                        <input 
-                            type="radio"
-                            name="profile"
-                            id="student"
-                            value="student"
-                            onChange={(e) => {setProfile(e.target.value)}}/>
-                        <label htmlFor="student"> &nbsp; Student &nbsp;</label>
-                        <input 
-                            type="radio"
-                            name="profile"
-                            id="tutor"
-                            value="tutor"
-                            onChange={(e) => {setProfile(e.target.value)}}/>
-                        <label htmlFor="tutor"> &nbsp;Tutor</label>
-                    </div>
-                </div>
-
-                <div className="signup_form">
-                    <label htmlFor="birth_date">What is your date of birth?</label>
-                    <input 
-                        type="date" 
-                        id="birth_date"
-                        value={birth_date}
-                        onChange={(e) => {setbirth_date(e.target.value)}} />
-
-                    <label htmlFor="email">Email</label>
-                    <input
-                        type="text"
-                        name="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => {setemail(e.target.value)}} />
-
-                    <label htmlFor="username">Username</label>
-                    <input 
-                        type="text"
-                        name="username"
-                        id="username"
-                        value={Name}
-                        onChange={(e) => {setName(e.target.value)}}
-                        />
-
-                        
-                    <label htmlFor="password">Password</label>
-                    <input 
-                        type="password"
-                        name="password"
-                        id="password"
-                        value={Password}
-                        onChange={(e) => {setPassword(e.target.value)}}
-                        /><br/>
-    
-                    <button className="signup_btn">Sign Up</button>
-                </div>
-              </form>
-
-                <NavLink to="/login">Already registered? Sign in</NavLink>
-            </div>
+          <p>
+            Join profese to get 1-on-1 online help for courses and assignments.
+          </p>
+          <p>
+            By logging into profese, you agree to our{' '}
+            <a href="#">Terms of Use</a> and <a href="#">Privacy Policy.</a>
+          </p>
         </div>
-        <Footer/>
-        </>
-    )
+
+        <div className="signup_sec2">
+          <form onSubmit={register}>
+            <div className="signup_profile_choice">
+              <label htmlFor="profile">Join profese as a</label>
+              <div className="choices">
+                <input
+                  type="radio"
+                  name="profile"
+                  id="student"
+                  value="student"
+                  onChange={(e) => {
+                    setProfile(e.target.value);
+                  }}
+                />
+                <label htmlFor="student"> &nbsp; Student &nbsp;</label>
+                <input
+                  type="radio"
+                  name="profile"
+                  id="tutor"
+                  value="tutor"
+                  onChange={(e) => {
+                    setProfile(e.target.value);
+                  }}
+                />
+                <label htmlFor="tutor"> &nbsp;Tutor</label>
+              </div>
+            </div>
+
+            <div className="signup_form">
+              <label htmlFor="birth_date">What is your date of birth?</label>
+              <input
+                type="date"
+                id="birth_date"
+                value={birth_date}
+                onChange={(e) => {
+                  setbirth_date(e.target.value);
+                }}
+              />
+
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                name="email"
+                id="email"
+                value={email}
+                onChange={(e) => {
+                  setemail(e.target.value);
+                }}
+              />
+
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                name="username"
+                id="username"
+                value={name}
+                onChange={(e) => {
+                  setName(e.target.value);
+                }}
+              />
+
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                name="password"
+                id="password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+              <br />
+
+              <button onClick={register} className="signup_btn">
+                Sign Up
+              </button>
+            </div>
+          </form>
+
+          <NavLink to="/login">Already registered? Sign in</NavLink>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
 }
