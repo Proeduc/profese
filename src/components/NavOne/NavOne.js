18dc1a12195a8ed5d3647b34089f80290b8d28c7
Ritika Dey <ritidey098@gmail.com>
2021-08-12 18:02:55 +0530

    Navbar updated

--- src/components/NavOne/NavOne.js
+++ src/components/NavOne/NavOne.js
@@ -6,33 +6,25 @@ export default function NavOne() {
     const [Search, setSearch] = useState("")
 
     return (
-        <div className="navone">
-            <div className="navone_content">
-                <div className="navone_icons">
-                   <NavLink to="#" target="_blank"><i className="fa fa-twitter"></i></NavLink>
-                   <NavLink to="#" target="_blank"><i className="fa fa-facebook-f"></i></NavLink>
-                   <NavLink to="#" target="_blank"><i className="fa fa-instagram"></i></NavLink>
+        <nav className="navbar navbar-inverse">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                    <ul className="nav navbar-nav">
+                        <li><NavLink to="#"><i className="fa fa-twitter"></i></NavLink></li>
+                        <li><NavLink to="#"><i className="fa fa-facebook-f"></i></NavLink></li>
+                        <li><NavLink to="#"><i className="fa fa-instagram"></i></NavLink></li>
+                    </ul>
                 </div>
-                
-                <div className="navone_links">
-                    <NavLink className="link" to="/">REVIEWS</NavLink>
-                    <NavLink className="link" to="/">BLOG</NavLink>
-                    <NavLink className="link" to="/">DISCUSSION BOARD</NavLink>
-                </div>
-            </div>
-            <div className="navone_search">
-                <NavLink to="#" target="_blank"><i className="fa fa-user"></i></NavLink>
 
-                <input 
-                   type="text"
-                   className="navone_searchbar"
-                   placeholder="Search.."
-                   value ={Search}
-                   onChange={(e) => {setSearch(e.target.value)}} />
-
-                <button className="navone_button"><i className="fa fa-search"></i></button>
+                <ul className="nav navbar-nav">
+                    <li><NavLink to="/">HOME</NavLink></li>
+                    <li><NavLink to="#">BLOGS</NavLink></li>
+                    <li><NavLink to="#">DISCUSSION BOARD</NavLink></li>
+                    <li><NavLink to="#">ASK A TUTOR</NavLink></li>
+                    <li><NavLink to="#">REVIEWS</NavLink></li>
+                </ul>
             </div>
-        </div>  
+        </nav> 
     )
 }
 
--- src/components/NavTwo/NavTwo.js
+++ src/components/NavTwo/NavTwo.js
@@ -4,46 +4,74 @@ import './NavTwo.css'
 
 export default function NavTwo() {
     return (
-        <div className="navtwo">
+    <nav class="navbar navtwo">
+        <div class="container-fluid">
+             <ul class="nav navbar-nav">
+                 <li class="dropdown">
+                     <NavLink class="dropdown-toggle" data-toggle="dropdown" to="#">Courses &nbsp;<span className="caret"></span></NavLink>
+                     <ul class="dropdown-menu">
+                        <li><NavLink to="#">Mathematics</NavLink></li>
+                        <li><NavLink to="#">Science</NavLink></li>
+                        <li><NavLink to="#">English</NavLink></li>
+                     </ul>
+                 </li>
+                 <li><NavLink to="#">Services</NavLink></li> 
+                 <li><NavLink to="#">Careers at Profese</NavLink></li>
+             </ul> 
 
-            <div className="navtwo_section">
+             <ul class="logo nav navbar-nav navbar-center">
+                 <li><img class="img-responsive" style={{height: "60px"}} src="https://profilersuzanne.com/wp-content/uploads/2018/02/logo-dummy.png" alt="logo"/></li>
+             </ul>
 
-                <div className="navtwo_courses">
-                    <button className="navtwo_dropdn">Courses</button>
-                    <div className="dropdn-content">
-                        {/* here we fetch data from backend and replace */}
-                         <p>Mathematics: </p>
-                         <NavLink to="/">Algebra</NavLink >
-                         <NavLink to="/">Geometry</NavLink >
-                         <NavLink to="/">Calculus</NavLink >
-                    </div>
-                </div>
-
-                <div className="navtwo_services">
-                    <NavLink className="link" to="/">Services</NavLink>
-                </div>
-
-                <div className="navtwo_careers">
-                    <NavLink className="link" to="/">Careers at Profese</NavLink>
-                </div>
-            </div>
-
-            <div className="navtwo_logo">
-                <NavLink to="/">logo img</NavLink>
-            </div>
-
-            <div className="navtwo_section">
-                <div className="navtwo_support">
-                    <NavLink className="link" to="/">Support</NavLink>
-                </div>
-                <div className="navtwo_login">
-                    <NavLink className="link" to="/login">Login</NavLink>
-                </div>
-                <div className="navtwo_signup">
-                    <NavLink className="link" to="/signup">SignUp</NavLink>
-                </div>
-            </div>
-
+             <ul class="nav navbar-nav navbar-right">
+                 <li><NavLink to="#">Support</NavLink></li>
+                 <li><NavLink to="/login">Login</NavLink></li>
+                 <li><NavLink to="/signup">Signup</NavLink></li>
+             </ul>
         </div>
+     </nav>
     )
 }
+
+
+// <div className="navtwo">
+
+// <div className="navtwo_section">
+
+//     <div className="navtwo_courses">
+//         <button className="navtwo_dropdn">Courses</button>
+//         <div className="dropdn-content">
+//             {/* here we fetch data from backend and replace */}
+//              <p>Mathematics: </p>
+//              <NavLink to="/">Algebra</NavLink >
+//              <NavLink to="/">Geometry</NavLink >
+//              <NavLink to="/">Calculus</NavLink >
+//         </div>
+//     </div>
+
+//     <div className="navtwo_services">
+//         <NavLink className="link" to="/">Services</NavLink>
+//     </div>
+
+//     <div className="navtwo_careers">
+//         <NavLink className="link" to="/">Careers at Profese</NavLink>
+//     </div>
+// </div>
+
+// <div className="navtwo_logo">
+//     <NavLink to="/">logo img</NavLink>
+// </div>
+
+// <div className="navtwo_section">
+//     <div className="navtwo_support">
+//         <NavLink className="link" to="/">Support</NavLink>
+//     </div>
+//     <div className="navtwo_login">
+//         <NavLink className="link" to="/login">Login</NavLink>
+//     </div>
+//     <div className="navtwo_signup">
+//         <NavLink className="link" to="/signup">SignUp</NavLink>
+//     </div>
+// </div>
+
+// </div>
