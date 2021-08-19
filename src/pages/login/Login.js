b8a0b8fedc5cdba21206c97ed77b7f4728f533b9
Ritika Dey <ritidey098@gmail.com>
2021-08-19 11:14:18 +0530

    signup and login form update

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -69,57 +69,56 @@ function Login() {
   };
   return (
     <>
-      <div className="login">
-        <div className="login_text">
-          <h1>Welcome back!</h1>
-          <p>
-            By logging into profese, you agree to our{' '}
-            <a href="#">Terms of Use</a> and <a href="#">Privacy Policy.</a>
-          </p>
-        </div>
+       <div className="login container">
+        <div className="row">
+           <div className="login_text col-lg-6 col-md-6 col-sm-6 col-xs-12">
+               <h1>Welcome back!</h1>
+               <h4>By logging into Profese you agree to our <a href="#">Terms of use</a> and <a href="#">Privacy Policy</a></h4>
+           </div>
+           <div className="login_input col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                <div className="social_login container-fluid">
+                    <button className="btn g_btn" onClick={logInGoogle}>Continue with Google</button>
+                    <button className="btn f_btn" onClick={logInFacebook}>Continue with Facebook</button>
+                </div>
+                <div className="manual_login container-fluid">
+                    <label htmlFor="email">Email</label>
+                    <input
+                    type="text"
+                    name="email"
+                    id="email" 
+                    className="form-control"
+                    value={mail}
+                    onChange={(e) => {
+                      setMail(e.target.value);
+                    }}
+                    />
 
-        <div className="login_input">
-          <div className="social_login">
-            <button className="social_btn" onClick={logInGoogle}>Continue with Google</button>
-            <button className="social_btn" onClick={logInFacebook}>Continue with Facebook</button>
-          </div>
+                    <label htmlFor="password">Password</label>
+                    <input
+                    type="password"
+                    name="password"
+                    id="password" 
+                    className="form-control"
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                    />
 
-          <div className="manual_login">
-            <label htmlFor="username">Email</label>
-            <input
-              type="text"
-              name="username"
-              id="username"
-              value={mail}
-              onChange={(e) => {
-                setMail(e.target.value);
-              }}
-            />
+                    <label>Forgot Password?</label>
+                    <button onClick={logIn} className="login_btn btn btn-block btn-primary">Log in</button>
 
-            <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
-
-            <label>Forgot Password?</label>
-            <button onClick={logIn} className="login_btn">
-              Log in
-            </button>
-
-            <NavLink to="/signup">Create an account</NavLink>
-          </div>
-        </div>
-      </div>
-
-      <Footer />
+                    <NavLink to="/signup">Create an account</NavLink>
+                </div>
+           </div>
+       </div>
+    </div>
+    <Footer />
     </>
   );
 }
 
 export default Login;
+ 
+
+  
--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -9,146 +9,138 @@ export default function Signup() {
   const [name, setName] = useState('');
   const [password, setPassword] = useState('');
   const [birth_date, setbirth_date] = useState('');
-  const [profile, setProfile] = useState('');
+  const [profile, setProfile] = useState(''); 
   const history = useHistory();
 
-  const validate = () => {
-    const mail_reg =
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-    if (
-      birth_date === '' ||
-      email === '' ||
-      password === '' ||
-      profile === '' ||
-      name === ''
-    ) {
-      alert('Empty fields not allowed....!!!');
-      return false;
-    } else if (!mail_reg.test(email)) {
-      alert('Enter valid email...!!!');
-      return false;
-    } else if (password.length < 8) {
-      alert('Password length must be greater than 8...!!!');
-      return false;
-    } else return true;
-  };
-
+  const validate = () => { 
+    var regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(password.length < 8 || !regexp.test(String(email).toLowerCase())) return false
+    else return true
+  }
   const register = (e) => {
-    e.preventDefault();
-    validate(); // for input validation
+    e.preventDefault(); 
 
     // post request if validate is true
-    auth.createUserWithEmailAndPassword(email, password).then((cred) => {
-      return db.collection('users').doc(cred.user.uid).collection('data').add({
-        email: email,
-        id: cred.user.uid,
-        birth_date: birth_date,
-        profile: profile,
-        name: name,
+    if(validate()){
+      auth.createUserWithEmailAndPassword(email, password).then((cred) => {
+        return db.collection('users').doc(cred.user.uid).collection('data').add({
+          email: email,
+          id: cred.user.uid,
+          birth_date: birth_date,
+          profile: profile,
+          name: name,
+        });
       });
-    });
-    // redirect to home page
-    history.push('/profile');
+      // redirect to home page
+      history.push('/profile');
+    }
+    else{
+      alert("Enter valid credentials")
+    }
   };
   return (
     <>
-      <div className="signup">
-        <div className="signup_sec1">
-          <h1>ENHANCE YOUR LEARNING AND EXPERIENCE, ANYTIME, ANYWHERE</h1>
+      <div className="signup container">
+       <div className="row">
+           <div className="signup_text col-lg-6 col-md-6 col-sm-6 col-xs-12"> 
+                <h1>ENHANCE YOUR LEARNING AND EXPERIENCE, ANYTIME, ANYWHERE</h1>
+                <h4>
+                Join profese to get 1-on-1 online help for courses and assignments.
+                </h4>
+                <h4>
+                By logging into profese, you agree to our
+                <a href="#">Terms of Use</a> and <a href="#">Privacy Policy.</a>
+                </h4>
+           </div>
+           <div className="signup_input col-lg-6 col-md-6 col-sm-6 col-xs-12">
+               <form onSubmit={register}>
+                <div className="manual_signup container-fluid">
+                    <div className="signup_profile_choice">
+                        <label htmlFor="profile">Join profese as a</label>
+                        <div className="choices"> 
+                          <input 
+                            type="radio"
+                            name="profile"
+                            id="student"
+                            value="Student"
+                            onChange={(e) => setProfile(e.target.value)}
+                            required
+                          />
+                          <label htmlFor="student" style={{color:"black"}}> &nbsp; Student &nbsp;</label>
+                          <input 
+                            type="radio"
+                            name="profile"
+                            id="tutor" 
+                            value="Tutor"
+                            onChange={(e) => setProfile(e.target.value)}
+                          />
+                          <label htmlFor="tutor" style={{color:"black"}}> &nbsp;Tutor</label>
+                        </div>
+                    </div>
 
-          <p>
-            Join profese to get 1-on-1 online help for courses and assignments.
-          </p>
-          <p>
-            By logging into profese, you agree to our{' '}
-            <a href="#">Terms of Use</a> and <a href="#">Privacy Policy.</a>
-          </p>
-        </div>
+                    <label htmlFor="birth_date">What is your date of birth?</label>
+                    <input
+                    type="date"
+                    id="birth_date" 
+                    className="form-control"
+                    value={birth_date}
+                    onChange={(e) => {
+                      setbirth_date(e.target.value);
+                    }}
+                    required
+                    />
+                      
+                    <label htmlFor="email">Email</label>
+                    <input
+                    type="text"
+                    name="email"
+                    id="email" 
+                    className="form-control"
+                    value={email}
+                    onChange={(e) => {
+                      setemail(e.target.value);
+                    }}  
+                    required
+                    />
 
-        <div className="signup_sec2">
-          <form onSubmit={register}>
-            <div className="signup_profile_choice">
-              <label htmlFor="profile">Join profese as a</label>
-              <div className="choices">
-                <input
-                  type="radio"
-                  name="profile"
-                  id="student"
-                  value="student"
-                  onChange={(e) => {
-                    setProfile(e.target.value);
-                  }}
-                />
-                <label htmlFor="student"> &nbsp; Student &nbsp;</label>
-                <input
-                  type="radio"
-                  name="profile"
-                  id="tutor"
-                  value="tutor"
-                  onChange={(e) => {
-                    setProfile(e.target.value);
-                  }}
-                />
-                <label htmlFor="tutor"> &nbsp;Tutor</label>
-              </div>
-            </div>
+                    <label htmlFor="username">Username</label>
+                    <input
+                    type="text"
+                    name="username"
+                    id="username" 
+                    className="form-control"
+                    value={name}
+                    onChange={(e) => {
+                      setName(e.target.value);
+                    }}
+                    required
+                    />
 
-            <div className="signup_form">
-              <label htmlFor="birth_date">What is your date of birth?</label>
-              <input
-                type="date"
-                id="birth_date"
-                value={birth_date}
-                onChange={(e) => {
-                  setbirth_date(e.target.value);
-                }}
-              />
+                    <label htmlFor="password">Password</label>
+                    <input
+                    type="password"
+                    name="password"
+                    id="password" 
+                    className="form-control"
+                    value={password}
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                    required
+                    /> 
+                    <button onClick="register" className="signup_btn btn btn-block btn-primary">Sign Up</button>
 
-              <label htmlFor="email">Email</label>
-              <input
-                type="text"
-                name="email"
-                id="email"
-                value={email}
-                onChange={(e) => {
-                  setemail(e.target.value);
-                }}
-              />
-
-              <label htmlFor="username">Username</label>
-              <input
-                type="text"
-                name="username"
-                id="username"
-                value={name}
-                onChange={(e) => {
-                  setName(e.target.value);
-                }}
-              />
-
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-              />
-              <br />
-
-              <button onClick={register} className="signup_btn">
-                Sign Up
-              </button>
-            </div>
-          </form>
-
-          <NavLink to="/login">Already registered? Sign in</NavLink>
-        </div>
+                    <NavLink to="/login">Log In here</NavLink>
+                </div>
+               </form>
+           </div>
+       </div>
       </div>
       <Footer />
     </>
   );
 }
+
+
+
+ 
