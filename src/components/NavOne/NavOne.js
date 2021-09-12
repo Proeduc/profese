2f784e3ed1bad0cefc28fa85e98a80fa21c6a848
RitZ <71270153+Ritika-dey@users.noreply.github.com>
2021-09-12 14:54:58 +0530

    Links added

--- src/components/NavOne/NavOne.js
+++ src/components/NavOne/NavOne.js
@@ -10,10 +10,10 @@ export default function NavOne() {
             <div className="container-fluid">
                 <div className="navbar-header">
                     <ul className="nav navbar-nav">
-                        <li><NavLink to="#"><i className="fa fa-linkedin"></i></NavLink></li>
+                        <li><NavLink to="https://www.linkedin.com/company/71097398/admin/"><i className="fa fa-linkedin"></i></NavLink></li>
                         <li><NavLink to="#"><i className="fa fa-whatsapp"></i></NavLink></li>
-                        <li><NavLink to="#"><i className="fa fa-facebook-f"></i></NavLink></li>
-                        <li><NavLink to="#"><i className="fa fa-instagram"></i></NavLink></li>
+                        <li><NavLink to="https://www.facebook.com/profeseducation"><i className="fa fa-facebook-f"></i></NavLink></li>
+                        <li><NavLink to="https://www.instagram.com/invites/contact/?i=d4i696sm61vp&utm_content=i8mwrkz"><i className="fa fa-instagram"></i></NavLink></li>
                     </ul>
                 </div>
 
