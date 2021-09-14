efbdbd3dfa0e96f6c690dece599e3e7e603214c0
Kacper <hernackikacper@gmail.com>
2021-09-14 10:32:01 +0200

    reviews

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -18,11 +18,15 @@ function Home() {
                 Excel in your course with personalized 1-on-1 learning and help
                 of our skilled subject experts.
               </h3>
-              <button className="btn btn-primary home__banner__btn">FIND A TUTOR</button>
+              <button className="btn btn-primary home__banner__btn">
+                FIND A TUTOR
+              </button>
             </div>
           </Slide>
           <div className="home_down">
-            <p className="home__down__text">See how personalized learning can work for you</p>
+            <p className="home__down__text">
+              See how personalized learning can work for you
+            </p>
             <button
               className="home_btn_down"
               onClick={() => {
@@ -51,7 +55,6 @@ function Home() {
           </form>
         </div>
         <Services />{' '}
-
         <Fade>
           <div className="container-fluid lesson_details">
             <h1>Online lessons. Just for you</h1>
@@ -72,86 +75,97 @@ function Home() {
                 </h3>
               </div>
               <div className="lesson_details_image col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                <img className="img-responsive" src={image} alt="image" />
+                <img
+                  alt=""
+                  className="img-responsive"
+                  src={image}
+                  alt="image"
+                />
               </div>
             </div>
           </div>
         </Fade>
-           
         <div className="reviews__home container-fluid">
+          {' '}
+          <h1 style={{ textAlign: 'center', marginBottom: '50px' }}>Reviews</h1>
           <div className="reviews_row__home container">
-             <div className="review__home">
-                <div className="reviewer_profile row">
-                    <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                      <img
-                        className="profile_img img-responsive img-circle"
-                        alt=""
-                        src="https://picsum.photos/id/237/100/90"
-                      ></img>
-                    </div>
-                    <div className="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                      <h3 className="username">User Name</h3>
-                      <p>Student at XYZ University</p>
-                    </div>
+            <div className="review__home">
+              <div className="reviewer_profile row">
+                <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                  <img
+                    className="profile_img img-responsive img-circle"
+                    alt=""
+                    src="https://picsum.photos/id/237/100/90"
+                  ></img>
                 </div>
-                <div className="review_content">
-                    <p>
-                      Lorem Ipsum is simply dummy text of the printing and
-                      typesetting industry. Lorem Ipsum has been the industry's
-                      standard dummy text ever since the 1500s.
-                    </p>
-                </div>
-             </div>
-
-             <div className="review__home">
-                <div className="reviewer_profile row">
-                    <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                      <img
-                        className="profile_img img-responsive img-circle"
-                        alt=""
-                        src="https://picsum.photos/id/237/100/90"
-                      ></img>
-                    </div>
-                    <div className="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                      <h3 className="username">User Name</h3>
-                      <p>Student at XYZ University</p>
-                    </div>
-                </div>
-                <div className="review_content">
-                    <p>
-                      Lorem Ipsum is simply dummy text of the printing and
-                      typesetting industry. Lorem Ipsum has been the industry's
-                      standard dummy text ever since the 1500s.
-                    </p>
-                </div>
-             </div>
-
-             <div className="review__home">
-                <div className="reviewer_profile row">
-                    <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4">
-                      <img
-                        className="profile_img img-responsive img-circle"
-                        alt=""
-                        src="https://picsum.photos/id/237/100/90"
-                      ></img>
-                    </div>
-                    <div className="col-lg-8 col-md-8 col-sm-8 col-xs-8">
-                      <h3 className="username">User Name</h3>
-                      <p>Student at XYZ University</p>
-                    </div>
-                </div>
-                <div className="review_content">
-                    <p>
-                      Lorem Ipsum is simply dummy text of the printing and
-                      typesetting industry. Lorem Ipsum has been the industry's
-                      standard dummy text ever since the 1500s.
-                    </p>
+                <div className="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                  <h3 className="username">User Name</h3>
+                  <p>Student at XYZ University</p>
                 </div>
               </div>
-
-              <div className="view_more">
-                <NavLink className="view__more__btn btn btn-primary" to='/reviews'>More &nbsp;<i class="glyphicon glyphicon-arrow-right"></i></NavLink>
+              <div className="review_content">
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s.
+                </p>
               </div>
+            </div>
+
+            <div className="review__home">
+              <div className="reviewer_profile row">
+                <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                  <img
+                    className="profile_img img-responsive img-circle"
+                    alt=""
+                    src="https://picsum.photos/id/237/100/90"
+                  ></img>
+                </div>
+                <div className="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                  <h3 className="username">User Name</h3>
+                  <p>Student at XYZ University</p>
+                </div>
+              </div>
+              <div className="review_content">
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s.
+                </p>
+              </div>
+            </div>
+
+            <div className="review__home">
+              <div className="reviewer_profile row">
+                <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                  <img
+                    className="profile_img img-responsive img-circle"
+                    alt=""
+                    src="https://picsum.photos/id/237/100/90"
+                  ></img>
+                </div>
+                <div className="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+                  <h3 className="username">User Name</h3>
+                  <p>Student at XYZ University</p>
+                </div>
+              </div>
+              <div className="review_content">
+                <p>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s.
+                </p>
+              </div>
+            </div>
+
+            <div className="view_more">
+              <NavLink
+                className="view__more__btn btn btn-primary"
+                to="/reviews"
+              >
+                More &nbsp;<i class="glyphicon glyphicon-arrow-right"></i>
+              </NavLink>
+            </div>
           </div>
         </div>
       </div>
