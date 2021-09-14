ce101ef4ce970dcaacb71e2785821776aaaa4d1c
Kacper <hernackikacper@gmail.com>
2021-09-14 10:04:38 +0200

    profile centered

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -37,9 +37,9 @@ const Profile = () => {
   } else {
     return (
       <>
-        <div className="user container-fluid">
-          <div className="user_details col-lg-3 col-md-3 col-sm-3 col-xs-12">
-            <div className="user_details_content">
+        <div className="user">
+          <div className="user__container">
+            <div className="user_details">
               <img
                 className="profile_pic img-responsive"
                 src={`${user?.photo}`}
@@ -56,38 +56,42 @@ const Profile = () => {
                   <i className="fa fa-graduation-cap">&nbsp;&nbsp;</i>UX Design
                 </p>
               </div>
-              <button className="msg_user_btn btn">
-                Message {user?.displayName}
-              </button>
+              <button className="msg_user_btn btn">Message</button>
             </div>
-          </div>
 
-          <div className="user_overview col-lg-offset-1 col-md-offset-1 col-lg-7 col-md-7 col-sm-7 col-xs-12">
-            <div className="user_overview_content">
-              <p>Overview</p>
-              <div className="row">
-                <div className="col-lg-4 col-md-4 col-sm-4 col-xs-6">
-                  <h4>
-                    <i className="fa fa-star"></i>&nbsp;5 Reviews
-                  </h4>
-                </div>
-                <div className="col-lg-4 col-md-4 col-sm-4 col-xs-6">
-                  <h4 className="user__overviewDescription">{description}</h4>
+            <div className="user_overview">
+              <div className="user_overview_content">
+                <p>Overview</p>
+                <div className="row">
+                  <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <h4>
+                      <i class="fa fa-star"></i>
+                      <i class="fa fa-star"></i>
+                      <i class="fa fa-star"></i>
+                      <i class="fa fa-star"></i>
+                      <div className="profile__reviews">5 Reviews</div>
+                    </h4>
+                  </div>
+                  <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <h4 className="user__overviewDescription">{description}</h4>
 
-                  {/* here i didn't got appropriate icons */}
-                </div>
-                <div className="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-                  <h4 className="user__overviewDescription">{specialities}</h4>
+                    {/* here i didn't got appropriate icons */}
+                  </div>
+                  <div className="col-lg-4 col-md-4 col-sm-4 col-xs-4">
+                    <h4 className="user__overviewDescription">
+                      {specialities}
+                    </h4>
 
-                  {/* here i didn't got appropriate icons */}
+                    {/* here i didn't got appropriate icons */}
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="user_about">
-              <p>About Me</p>
-              <div className="user_about_content">
-                <h4 className="user__overviewDescription">{about}</h4>
+              <div className="user_about">
+                <p>About Me</p>
+                <div className="user_about_content">
+                  <h4 className="user__overviewDescription">{about}</h4>
+                </div>
               </div>
             </div>
           </div>
