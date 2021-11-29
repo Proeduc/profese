afba5ab1283e96834e1f01a0e0cca8ac1f2f01aa
Ritika Dey <ritidey098@gmail.com>
2021-11-29 21:43:11 +0530

    Profile edit modal added

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -4,13 +4,15 @@ import { Link } from 'react-router-dom'
 import UserAns from './UserAns'
 import UserBasics from './UserBasics'
 import UserEdu from './UserEdu'
-import UserQues from './UserQues'
+import UserQues from './UserQues' 
+import Modal from 'react-modal'
  
  function Profile() {
    const [ShowBasics, setShowBasics] = useState(false)
    const [ShowAns, setShowAns] = useState(false)
    const [ShowEdu, setShowEdu] = useState(false)
    const [ShowQues, setShowQues] = useState(false)
+   const [IsModalOpen, setIsModalOpen] = useState(false)
 
    const changeBasics = () => {
     setShowBasics(true)
@@ -38,7 +40,7 @@ import UserQues from './UserQues'
     setShowAns(false)
     setShowEdu(false)
     setShowQues(true)
-   }
+   } 
    return (
      <>
        <div className="profile__container container">
@@ -67,8 +69,82 @@ import UserQues from './UserQues'
          {/* profile sidenav end */}
 
          {/* profile data */}
-          <div className="col-sm-8 profile__data"> 
-              <button style={{float:"right", borderRadius:"50%"}} className="btn btn-primary"><i className="fa fa-edit" aria-hidden="true"></i></button>
+          <div className="col-sm-8 profile__data">  
+              {/* modal content */} 
+              <button type="button" className="btn btn-info" data-toggle="modal" data-target="#profileModal" style={{float:"right", borderRadius:"50%"}}><i className="fa fa-edit" aria-hidden="true"></i></button>
+
+              {/* <!-- Modal --> */}
+              <div id="profileModal" className="modal fade" role="dialog">
+                <div className="modal-dialog">
+                  {/* <!-- Modal content--> */}
+                  <div className="modal-content" style={{backgroundColor:"#153280", color:"white"}}>
+                    <div className="modal-header" style={{backgroundColor:"white", color:"#153280", fontWeight:"bold"}}>
+                      <button type="button" className="close" data-dismiss="modal">&times;</button>
+                      <h4 className="modal-title">Edit your profile details</h4>
+                    </div>
+                    <div className="modal-body">
+                        <div className="form-group">
+                          <label htmlFor="name">Name:</label>
+                          <input 
+                              type="text" 
+                              name="name"   
+                              className="form-control" 
+                              placeholder="Update your name here" required/>
+                        </div>
+
+                        <div className="form-group">
+                          <label htmlFor="email">Email:</label>
+                          <input 
+                              type="text" 
+                              name="email"   
+                              className="form-control" 
+                              placeholder="Update your email here" required/>
+                        </div>
+
+                        <div className="form-group">
+                          <label htmlFor="dob">DOB:</label>
+                          <input 
+                              type="date" 
+                              name="DOB"  
+                              className="form-control" 
+                              placeholder="Update your Date of birth here" required/>
+                        </div>
+
+                        <div className="form-group">
+                          <label htmlFor="course">Course:</label>
+                          <input 
+                              type="text" 
+                              name="course"  
+                              className="form-control" 
+                              placeholder="Update your Course here" required/>
+                        </div>
+
+                        <div className="form-group">
+                          <label htmlFor="major">Major:</label>
+                          <input 
+                              type="text" 
+                              name="major"   
+                              className="form-control" 
+                              placeholder="Update your major subject here" required/>
+                        </div>
+
+                        <div className="form-group">
+                          <label htmlFor="institution">Institution:</label>
+                          <input 
+                              type="text" 
+                              name="institution"  
+                              className="form-control" 
+                              placeholder="Update your institution here" required/>
+                        </div>
+
+
+                        <button className="btn btn-default">Update</button>
+                    </div> 
+                  </div>
+                </div>
+              </div>
+            
+              
               { 
                 ShowBasics ? <UserBasics /> : (ShowEdu ? <UserEdu /> : (ShowAns ? <UserAns /> : ShowQues ? <UserQues /> : <UserBasics />))
               }
