db6a993e176cea020c108fef72ddfc264b4e638a
Ritika Dey <ritidey098@gmail.com>
2021-12-27 18:07:26 +0530

    discussion board updates

--- src/pages/discussion/Discussion.js
+++ src/pages/discussion/Discussion.js
@@ -70,40 +70,46 @@ import "./Discussion.css"
                          <h3 className="text-primary">Top questions for you</h3>
                          <div className="discussion__ques">
                              <div className="discussion__que">
-                                 <h4 className="discussion__que__title">How do you use electricity to make something cold?</h4>
+                                 <div className='discussion__que__upper'>
+                                    <h4 className="discussion__que__title">How do you use electricity to make something cold?</h4>
+                                    <div className="discussion__que__likes text-success">5 <i className="fa fa-check-circle-o"></i></div>
+                                    <div className="discussion__que__dislikes text-danger">2 <i className="fa fa-close"></i></div>
+                                 </div>
                                  <div className="discussion__que__meta">
-                                     <div className="discussion__que__anscnt text-primary"><p><i className="fa fa-edit " ></i> No answers yet</p></div>
-                                     <div className="discussion__que__addans text-primary"><p><i className="fa fa-plus" aria-hidden="true"></i><Link to="/add-ans"> Add a answer</Link></p></div>
-                                     <div className="discussion__que__anscnt text-primary"><i className="fa fa-tag" aria-hidden="true"></i> Tag</div>
-                                     <div className="discussion__que__likes text-success">5 <i className="fa fa-check-circle-o"></i></div>
-                                     <div className="discussion__que__dislikes text-danger">2 <i className="fa fa-close"></i></div>
+                                    <div className="discussion__que__anscnt text-primary"><p><i className="fa fa-edit " ></i> No answers yet</p></div>
+                                    <div className="discussion__que__addans text-primary"><p><i className="fa fa-plus" aria-hidden="true"></i><Link to="/add-ans"> Add a answer</Link></p></div>
+                                    <div className="discussion__que__anscnt text-primary"><i className="fa fa-tag" aria-hidden="true"></i> Tag</div>
                                  </div>
                              </div>
 
                              <div className="discussion__que">
-                                 <h4 className="discussion__que__title">How do you use electricity to make something cold?</h4>
-                                 <img 
-                                  className="img-responsive img-rounded"
-                                  src="https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?cs=srgb&dl=pexels-jaime-reimer-2662116.jpg&fm=jpg" 
-                                  placeholder="ques banner">
-                                 </img>
+                                <div className='discussion__que__upper'>
+                                    <h4 className="discussion__que__title">How do you use electricity to make something cold?</h4>
+                                    <div className="discussion__que__likes text-success">5 <i className="fa fa-check-circle-o"></i></div>
+                                    <div className="discussion__que__dislikes text-danger">2 <i className="fa fa-close"></i></div>
+                                </div>
+                                <img 
+                                    className="img-responsive img-rounded"
+                                    src="https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?cs=srgb&dl=pexels-jaime-reimer-2662116.jpg&fm=jpg" 
+                                    placeholder="ques banner">
+                                </img>
                                  <div className="discussion__que__meta">
                                      <div className="discussion__que__anscnt text-primary"><p><i className="fa fa-edit " ></i> 5 answers yet</p></div>
                                      <div className="discussion__que__addans text-primary"><p><i className="fa fa-plus" aria-hidden="true"></i><Link to="/add-ans"> Add a answer</Link></p></div>
                                      <div className="discussion__que__anscnt text-primary"><i className="fa fa-tag" aria-hidden="true"></i> Tag</div>
-                                     <div className="discussion__que__likes text-success">5 <i className="fa fa-check-circle-o"></i></div>
-                                     <div className="discussion__que__dislikes text-danger">2 <i className="fa fa-close"></i></div>
                                  </div>
                              </div>
 
                              <div className="discussion__que">
-                                 <h4 className="discussion__que__title">How do you use electricity to make something cold?</h4>
+                                <div className='discussion__que__upper'>
+                                    <h4 className="discussion__que__title">How do you use electricity to make something cold?</h4>
+                                    <div className="discussion__que__likes text-success">5 <i className="fa fa-check-circle-o"></i></div>
+                                    <div className="discussion__que__dislikes text-danger">2 <i className="fa fa-close"></i></div>
+                                </div>
                                  <div className="discussion__que__meta">
                                      <div className="discussion__que__anscnt text-primary"><p><i className="fa fa-edit " ></i> No answers yet</p></div>
                                      <div className="discussion__que__addans text-primary"><p><i className="fa fa-plus" aria-hidden="true"></i><Link to="/add-ans"> Add a answer</Link></p></div>
                                      <div className="discussion__que__anscnt text-primary"><i className="fa fa-tag" aria-hidden="true"></i> Tag</div>
-                                     <div className="discussion__que__likes text-success">5 <i className="fa fa-check-circle-o"></i></div>
-                                     <div className="discussion__que__dislikes text-danger">2 <i className="fa fa-close"></i></div>
                                  </div>
                              </div>
                          </div>
