1beebb0ccf1c12849b434491808cca65d8784dbe
Ritika Dey <ritidey098@gmail.com>
2022-01-20 17:36:23 +0530

    Admin panel changes for exam and live sessions

--- src/pages/Admin/ReqExam.js
+++ src/pages/Admin/ReqExam.js
@@ -19,6 +19,8 @@ function ReqExam() {
                             <th>Image</th>
                             <th>Download</th>
                             <th>Status</th>
+                            <th>Add Question</th>
+                            <th>Add Answer</th>
                         </tr>
                     </thead>
 
@@ -33,6 +35,9 @@ function ReqExam() {
                             <td><img className="img-responsive" src="https://media.istockphoto.com/photos/the-student-life-picture-id862661268?b=1&k=20&m=862661268&s=170667a&w=0&h=SQ2yAt1TLD6zg_AiAbZdmApZ8N0CmfGRWE_c52LBd6w=" alt="assignment image" width="40px" /></td>
                             <td><Link to="#">Click here</Link></td>
                             <td>Done</td>
+                            {/* add that id logic here */}
+                            <td><button className='btn btn-primary' data-toggle="modal" data-target="#questionModal">Add Question</button></td> 
+                            <td><button className='btn btn-primary' data-toggle="modal" data-target="#answerModal">Add Answer</button></td>
                         </tr>
 
 
@@ -45,10 +50,89 @@ function ReqExam() {
                             <td><img className="img-responsive" src="https://media.istockphoto.com/photos/the-student-life-picture-id862661268?b=1&k=20&m=862661268&s=170667a&w=0&h=SQ2yAt1TLD6zg_AiAbZdmApZ8N0CmfGRWE_c52LBd6w=" alt="assignment image" width="40px" /></td>
                             <td><Link to="#">Click here</Link></td>
                             <td>Pending</td>
+                            {/* add that id logic here */}
+                            <td><button className='btn btn-primary' data-toggle="modal" data-target="#questionModal">Add Question</button></td>
+                            <td><button className='btn btn-primary' data-toggle="modal" data-target="#answerModal">Add Answer</button></td>
                         </tr>
 
                     </tbody>
                 </table>
+
+
+                {/* <!-- Modal for ques start --> */} 
+                <div id="questionModal" className="modal fade" role="dialog">
+                <div className="modal-dialog">
+                  {/* <!-- Modal content--> */}
+                  <div className="modal-content">
+
+                    <div className="modal-header">
+                      <button type="button" className="close" data-dismiss="modal">&times;</button>
+                      <h4 className="modal-title">Add Question for Practice exam</h4>
+                    </div>
+
+                    <div className="modal-body">
+                        <div className="form-group">
+                          <label htmlFor="name">Price:</label>
+                          <input 
+                              type="number"
+                              name="price"   
+                              className="form-control" 
+                              placeholder="Add total price for this assignment" required/>
+                        </div>  
+
+                        <div className="form-group">
+                          <label htmlFor="remarks">Remarks</label>
+                          <input 
+                              type="text" 
+                              name="institution"  
+                              className="form-control" 
+                              placeholder="Add remarks if any, OPTIONAL"/>
+                        </div>
+
+                        <div className='form-group'>
+                            <label htmlFor='solutionfile'>Question File(PDF):</label>
+                            <input
+                              type="file"
+                              name='solutionfile'
+                              className='form-control' required/>
+                        </div>
+
+
+                        <button className="btn btn-primary">Post Solution</button>
+                    </div> 
+                  </div>
+                </div>
+              </div>
+              {/* modal for question end */}
+
+
+
+              {/* <!-- Modal for answer end --> */} 
+              <div id="answerModal" className="modal fade" role="dialog">
+                <div className="modal-dialog">
+                  {/* <!-- Modal content--> */}
+                  <div className="modal-content">
+
+                    <div className="modal-header">
+                      <button type="button" className="close" data-dismiss="modal">&times;</button>
+                      <h4 className="modal-title">Add solution for this Practice exam</h4>
+                    </div>
+
+                    <div className="modal-body">
+                        <div className='form-group'>
+                            <label htmlFor='solutionfile'>Answer File(PDF):</label>
+                            <input
+                              type="file"
+                              name='solutionfile'
+                              className='form-control' required/>
+                        </div> 
+                        <button className="btn btn-primary">Post Solution</button>
+                    </div> 
+                  </div>
+                </div>
+              </div>
+
+              {/* modal for answer end */}
             </div>
         </div>
         </>
--- src/pages/Admin/ReqSessions.js
+++ src/pages/Admin/ReqSessions.js
@@ -18,6 +18,7 @@ function ReqSessions() {
                             <th>Image</th>
                             <th>Duration</th>
                             <th>Status</th>
+                            <th>Add Meeting Link</th>
                         </tr>
                     </thead>
 
@@ -32,6 +33,8 @@ function ReqSessions() {
                             <td><img className="img-responsive" src="https://media.istockphoto.com/photos/the-student-life-picture-id862661268?b=1&k=20&m=862661268&s=170667a&w=0&h=SQ2yAt1TLD6zg_AiAbZdmApZ8N0CmfGRWE_c52LBd6w=" alt="assignment image" width="40px" /></td>
                             <td>45 Minutes</td>
                             <td>Done</td>
+                            {/* add that id logic here */}
+                            <td><button className='btn btn-primary' data-toggle="modal" data-target="#solutionModal">Add</button></td>
                         </tr>
 
 
@@ -44,10 +47,70 @@ function ReqSessions() {
                             <td><img className="img-responsive" src="https://media.istockphoto.com/photos/the-student-life-picture-id862661268?b=1&k=20&m=862661268&s=170667a&w=0&h=SQ2yAt1TLD6zg_AiAbZdmApZ8N0CmfGRWE_c52LBd6w=" alt="assignment image" width="40px" /></td>
                             <td>30 Minutes</td>
                             <td>Pending</td>
+                            {/* add that id logic here */}
+                            <td><button className='btn btn-primary' data-toggle="modal" data-target="#solutionModal">Add</button></td>
                         </tr>
 
                     </tbody>
                 </table>
+
+
+
+                {/* <!-- Modal start --> */} 
+              <div id="solutionModal" className="modal fade" role="dialog">
+                <div className="modal-dialog">
+                  {/* <!-- Modal content--> */}
+                  <div className="modal-content">
+
+                    <div className="modal-header">
+                      <button type="button" className="close" data-dismiss="modal">&times;</button>
+                      <h4 className="modal-title">Add solution for this assignment</h4>
+                    </div>
+
+                    <div className="modal-body">
+                        <div className="form-group">
+                          <label htmlFor="name">Price:</label>
+                          <input 
+                              type="number"
+                              name="price"   
+                              className="form-control" 
+                              placeholder="Add total price for this assignment" required/>
+                        </div>  
+
+                        <div className="form-group">
+                          <label htmlFor="remarks">Remarks</label>
+                          <input 
+                              type="text" 
+                              name="institution"  
+                              className="form-control" 
+                              placeholder="Add remarks if any, OPTIONAL"/>
+                        </div>
+
+                        <div className='form-group'>
+                            <label htmlFor='meeetinglink'>Meeting Link</label>
+                            <input
+                              type='text'
+                              name='meetinglink'
+                              className='form-control'
+                              placeholder='Add meeting link here'/>
+                        </div>
+
+                        <div className='form-group'>
+                            <label htmlFor='duration'>Duration</label>
+                            <input
+                              type='number'
+                              name='duration'
+                              className='form-control'
+                              placeholder='Add duration' />
+                        </div>
+
+                        <button className="btn btn-primary">Post Solution</button>
+                    </div> 
+                  </div>
+                </div>
+              </div>
+
+              {/* modal end */}
             </div>
         </div>
         </>
